ed5a83da1462dd8050a779b79874df72f63754a3
= <richardboomsma@gmail.com>
2016-05-12 02:10:10 +0200

    veel shit

---
+++ AgroApp/src/AgroApp/wwwroot/js/loginview.js
@@ -0,0 +1,7 @@
+﻿angular.module('AgroApp').
+    controller('loginController', function ($scope, $location) {
+        $scope.goLogin = function () {
+            $scope.showloading = true;
+            window.location.href = '/admin/main';
+        };
+    })
