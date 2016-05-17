8d232db6bf4033bc672783c651263c1313c01c7d
Ailco Geveke <ailcoharmen@live.nl>
2016-05-17 14:19:36 +0200

    Machine dingen

---
+++ AgroApp/src/AgroApp/Controllers/Admin/MachineController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AgroApp.Controllers.Admin
+{
+    public class MachineController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet("admin/machinebeheer")]
+        public IActionResult Index()
+        {
+            return View("../admin/machinebeheer/machinebeheer");
+        }
+
+        [HttpGet("admin/machinebeheer/toevoegen")]
+        public IActionResult MachineToevoegen()
+        {
+            return View("../admin/machinebeheer/machineadd");
+        }
+    }
+}
