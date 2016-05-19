b167c8fcc7d2e65de27d0ba0ec42b9f2bb850161
Ailco Geveke <ailcoharmen@live.nl>
2016-05-19 09:56:21 +0200

    Model Fix

--- AgroApp/src/AgroApp/Models/machine.cs
+++ AgroApp/src/AgroApp/Models/machine.cs
@@ -7,22 +7,21 @@ namespace AgroApp.Models
 {
     public class Machine { 
 
-        public int IdMachines { private set; get; }
+        public int IdMachine { private set; get; }
         public string Type { private set; get; }
         public int Nummer { private set; get; }
         public string Naam { private set; get; }
         public string Kenteken { private set; get; }
         public string Status { private set; get; }
-
-        public Machine(int idMachines, string type, int nummer, string naam, string kenteken, string status)
+        
+        public Machine(int idMachine = -1, string type = "", int nummer = 0, string naam="", string kenteken="", string status="")
         {
-            IdMachines = idMachines;
+            IdMachine = idMachine;
             Type = type;
             Nummer = nummer;
             Naam = naam;
             Kenteken = kenteken;
             Status = status;
         }
-
     }
 }
