6f9f6acad2e2677f5a3d34585d82e706b7ce082e
= <richardboomsma@gmail.com>
2016-05-26 01:15:10 +0200

    planning

---
+++ AgroApp/src/AgroApp/Models/Opdracht.cs
@@ -0,0 +1,17 @@
+﻿using MySql.Data.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgroApp.Models
+{
+    public class Opdracht
+    {
+        public Customer selectedKlant;
+        public User[] selectedGebruikers;
+        public string locatie;
+        public string beschrijving;
+        public DateTime datum;
+    }
+}
