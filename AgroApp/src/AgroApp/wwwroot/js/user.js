e6734ad150d6f55a47d5ccc0f649852607fe97bd
= <richardboomsma@gmail.com>
2016-05-25 14:47:08 +0200

    blablabla

--- AgroApp/src/AgroApp/wwwroot/js/user.js
+++ AgroApp/src/AgroApp/wwwroot/js/user.js
@@ -1,7 +1,7 @@
 ﻿var agroApp = angular.module('AgroApp');
 
 agroApp.controller('PlanningView', function ($scope) {
-
+    
 });
 
 agroApp.factory('getUsers', function () {
@@ -9,6 +9,19 @@ agroApp.factory('getUsers', function () {
         $http({
             method: 'GET',
             url: '/api/user/getallegebruikers',
+            params: 'limit=10, sort_by=created:desc',
+            headers: { 'Authorization': 'Token token=xxxxYYYYZzzz' }
+        }).success(function (data) {
+            return data;
+        })
+    }
+});
+
+agroApp.factory('getCustomers', function () {
+    return function () {
+        $http({
+            method: 'GET',
+            url: '/api/werkbon/getklanten',
             params: 'limit=10, sort_by=created:desc',
             headers: { 'Authorization': 'Token token=xxxxYYYYZzzz' }
         }).success(function (data) {
