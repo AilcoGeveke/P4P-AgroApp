62a6e2313ea33f114ec757a9996427f6eb5a68d9
albert1997 <albertvanhouten6@hotmail.com>
2016-07-13 14:56:35 +0200

    Models

---
+++ AgroApp/AgroWebApp/Models/Cargo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AgroApp.Models
+{
+    public class Assignment
+    {
+        public int IdAssignment { set; get; }
+        public string Location { set; get; }
+        public string Description { set; get; }
+        public DateTime Date { set; get; }
+
+        public Assignment() { }
+
+        public Assignment(DateTime date, int idAssignment = -1, string location = "", string description = "")
+
+        {
+            IdAssignment = idAssignment;
+            Location = location;
+            Description = description;
+            Date = date;
+        }
+    }
+}
