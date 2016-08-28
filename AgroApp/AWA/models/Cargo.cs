00ee568f2c6dbe9207969a710adf8452b73e0168
Richard Boomsma <richardboomsma@gmail.com>
2016-08-28 17:38:39 +0200

    lowercasing

---
+++ AgroApp/AWA/models/Cargo.cs
@@ -0,0 +1,13 @@
+﻿namespace AWA.Models
+{
+    public class Cargo
+    {
+        public int CargoId { set; get; }
+        public long Date { set; get; }
+        public string Type { set; get; }
+        public int FullLoad { set; get; }
+        public int EmptyLoad { set; get; }
+        public int NetLoad { set; get; }
+        public string Direction { set; get; }
+    }
+}
